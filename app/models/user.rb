5c9aa03af7d35a3161b291648e74418f6e22cc36
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-24 03:51:55 +0000

    Add validations for username, email, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  has_secure_password validations: false
+
+  validates :username, presence: true, length: {minimum: 3}
+  validates :email, presence: true, format: { with: /\A[\w\d]+[@][a-z]+.(com|co.uk)\z/, 
+    message: 'The email address you entered is not valid' }
+  validates :password, presence: true, length: {minimum: 8}, on: :create
 end
