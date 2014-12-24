fbed530314f2d456e8f185619f705b498cbf2fd7
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-24 22:14:36 +0000

    Add LeaderFollower model

---
+++ app/models/leader_follower.rb
@@ -0,0 +1,4 @@
+class LeaderFollower < ActiveRecord::Base
+  belongs_to :leader, class_name: 'User', foreign_key: 'leader_id'
+  belongs_to :follower, class_name: 'User', foreign_key: 'follower_id'
+end
