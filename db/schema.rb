5b67f97e04e96fb3abd10b3a33992f861529fa81
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-24 03:56:06 +0000

    Add migration for statuses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141224033545) do
+ActiveRecord::Schema.define(version: 20141224035341) do
+
+  create_table "statuses", force: true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
