1636372c9cd17cd685a768365299caca6cd4e73d
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-24 20:40:46 +0000

    Only validate email on create. Remove presence validations where min length ones appears

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
   has_many :statuses
 
-  validates :username, presence: true, length: {minimum: 3}
+  validates :username, length: {minimum: 3, message: 'Your username must have more than 2 characters'}
   validates :email, presence: true, format: { with: /\A[\w\d]+[@][a-z]+.(com|co.uk)\z/, 
-    message: 'The email address you entered is not valid' }
+    message: 'The email address you entered is not valid' }, on: :create
   validates :password, presence: true, length: {minimum: 8}, on: :create
 end
