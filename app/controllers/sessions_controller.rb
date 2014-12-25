80f73f295de18ea31f56836df0f9f15635a1e134
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-25 16:57:55 +0000

    Add logic for logging in and out in create and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,22 @@
 class SessionsController < ApplicationController
-  def new
-    
-  end
+  def new; end
 
   def create
+    user = User.find_by(username: params[:username])
 
+    if user && user.authenticate(params[:password])
+      flash[:notice] = 'You were logged in'
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:error] = "There's something wrong with your username or password"
+      render :new
+    end
   end
 
   def destroy
-
+    flash[:notice] = 'You successfully logged out'
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
