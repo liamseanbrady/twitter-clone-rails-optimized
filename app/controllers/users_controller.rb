45f8d48b976964f2180a2764e140d1956e4bf6a7
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-25 14:57:00 +0000

    Add create action and private user_params method for strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,29 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = 'You registered successfully'
+      redirect_to root_path
+    else
+      flash[:error] = 'Registration was unsuccessful'
+      render :new
+    end
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
