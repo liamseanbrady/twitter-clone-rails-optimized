a49c4bd2e532f9cc5fbc1b3291f2ff948f03197d
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-25 14:49:54 +0000

    Add time zone to comment example for datetime helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def display_datetime(dt)
-    dt.strftime("%m %b %Y, %H:%M %Z") # 25 May 2014, 23:12
+    dt.strftime("%m %b %Y, %H:%M %Z") # 25 May 2014, 23:12 UTC
   end
 end
