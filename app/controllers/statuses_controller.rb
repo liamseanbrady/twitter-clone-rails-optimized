3a9fc7f08c9458d2cf0b46cc77cf704737824fb9
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-25 19:07:26 +0000

    Create status and redirect or else render if status is not created

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -4,10 +4,23 @@ class StatusesController < ApplicationController
   end
 
   def create
+    @status = Status.new(status_params)
 
+    if @status.save
+      flash[:notice] = 'Your status was posted'
+      redirect_to user_path(@status.creator)
+    else
+      render :new
+    end
   end
 
   def show
 
+  end
+
+  private
+
+  def status_params
+    params.require(:status).permit(:body)
   end
 end
